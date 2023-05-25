457f4bcfc1650909ebddbeae985ea6cf4506af8b
jajang mahrul <jajangmahrul22@gmail.com>
2023-05-25 23:28:43 +0700

    add factory db-seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Post;
+use App\Models\User;
+use App\Models\Category;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +16,58 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        
+        // User::create([
+            //     'name' => 'Jajang Mahrul',
+            //     'email' => 'jajangmahrul22@gmail.com',
+            //     'password' => bcrypt('admin')
+            // ]);
+            
+            // User::create([
+                //     'name' => 'Azzario razy',
+                //     'email' => 'azza@gmail.com',
+                //     'password' => bcrypt('admin1')
+                // ]);
+        User::factory(3)->create();
+                
+        Category::create([
+            'name' => 'Programming',
+            'slug' => 'programming'
+        ]);
+
+        Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        Post::factory(20)->create();
+
+        // Post::create([
+        //     'title' => 'Judul Pertama',
+        //     'slug' => 'judul-pertama',
+        //     'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        //                 Harum cupiditate nulla omnis 
+        //                 laboriosam consequatur deleniti autem aspernatur voluptatum quaerat nobis?',
+        //     'body' => '<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi ipsa molestiae odio adipisci deleniti officia dolorum in 
+        //                 provident a aut minima perspiciatis tempore optio quia earum consequuntur reprehenderit distinctio,</p> 
+        //                 <p>labore dolorem quidem dicta, rem eligendi facilis itaque! 
+        //                 Eveniet corrupti odit dolore nobis excepturi quas quaerat corporis, eaque exercitationem at possimus?</p>',
+        //     'category_id' => 1,
+        //     'user_id' =>1
+        // ]);
+
+        // Post::create([
+        //     'title' => 'Judul Kedua',
+        //     'slug' => 'judul-kedua',
+        //     'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        //                 Harum cupiditate nulla omnis 
+        //                 laboriosam consequatur deleniti autem aspernatur voluptatum quaerat nobis?',
+        //     'body' => '<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi ipsa molestiae odio adipisci deleniti officia dolorum in 
+        //                 provident a aut minima perspiciatis tempore optio quia earum consequuntur reprehenderit distinctio,</p> 
+        //                 <p>labore dolorem quidem dicta, rem eligendi facilis itaque! 
+        //                 Eveniet corrupti odit dolore nobis excepturi quas quaerat corporis, eaque exercitationem at possimus?</p>',
+        //     'category_id' => 2,
+        //     'user_id' =>2
+        // ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\Post;
+use App\Models\User;
 use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
@@ -44,5 +45,12 @@ Route::get('/categories/{category:slug}', function(Category $category){
         'title' => $category->name,
         'posts' => $category->posts,
         'category' => $category->slug
+    ]);
+});
+
+Route::get('/authors/{author:username}', function(User $author){
+    return view('posts', [
+        'title' => 'User Posts',
+        'posts' => $author->posts
     ]);
 });
