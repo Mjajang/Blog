a71972cded198d7ea54a759e64f7d0134f5e6d08
jajang mahrul <jajangmahrul22@gmail.com>
2023-05-26 14:33:44 +0700

    implement N+1 Problem

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -10,7 +10,7 @@
           <a class="nav-link {{ $title == "Home" ? 'active' : '' }}" href="/">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link {{ $title == "Posts" ? 'active' : '' }}" href="/posts">Posts</a>
+          <a class="nav-link {{ $title == "All Posts" ? 'active' : '' }}" href="/posts">Posts</a>
         </li>
         <li class="nav-item">
           <a class="nav-link {{ $title == "About" ? 'active' : '' }}" href="/about">About</a>
